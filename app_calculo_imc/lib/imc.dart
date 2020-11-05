e5a67b6396f6f83b3f8fe5d14e3b77dcdaf237d4
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-11-05 12:09:58 -0300

    Finalizando app

--- app_calculo_imc/lib/imc.dart
+++ app_calculo_imc/lib/imc.dart
@@ -10,10 +10,59 @@ class _ImcState extends State<Imc> {
   // o underscore significa que o campo é privado.
   final TextEditingController _alturaController = TextEditingController();
   final TextEditingController _pesoController = TextEditingController();
+  final key = GlobalKey<ScaffoldState>();
+  var _resultado = '';
+  var _situacao = '';
+
+  _onItemTapped(int index) {
+    if (index == 0) {
+      _alturaController.clear();
+      _pesoController.clear();
+      setState(() {
+        _resultado = '';
+        _situacao = '';
+      });
+    } else if (_alturaController.text.isEmpty || _pesoController.text.isEmpty) {
+      key.currentState.showSnackBar(
+        SnackBar(content: Text("Altura e peso são obrigatórios")),
+      );
+    } else {
+      try {
+        var peso = double.parse(_pesoController.text);
+        var altura = double.parse(_alturaController.text);
+        var imc = peso / (altura * altura);
+        setState(() {
+          _resultado = 'Seu imc é: ${imc.toStringAsFixed(2)}';
+          if (imc < 17) {
+            _situacao = 'Muito abaixo do peso';
+          } else if ((imc >= 17) && (imc < 18.5)) {
+            _situacao = 'Abaixo do peso';
+          } else if ((imc >= 18.5) && (imc < 25)) {
+            _situacao = 'Peso normal';
+          } else if ((imc >= 25) && (imc < 30)) {
+            _situacao = 'Acima do peso';
+          } else if ((imc >= 30) && (imc < 35)) {
+            _situacao = 'Obesidade I';
+          } else if ((imc >= 35) && (imc < 40)) {
+            _situacao = 'Obesidade II (severa)';
+          } else {
+            _situacao = 'Obesidade III (mórbida)';
+          }
+        });
+      } catch (e) {
+        key.currentState.showSnackBar(
+          SnackBar(
+              content:
+                  Text("Altura ou peso foram informado em formato inválidos")),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: key,
       appBar: AppBar(
         title: Text("Cálculo IMC"),
         backgroundColor: Colors.indigo,
@@ -25,6 +74,7 @@ class _ImcState extends State<Imc> {
           TextField(
             //é a partir do controller que eu consigo manipular os dados dentro dele
             controller: _alturaController,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
             decoration: InputDecoration(
               hintText: "Altura",
               border:
@@ -35,6 +85,7 @@ class _ImcState extends State<Imc> {
           TextField(
             //é a partir do controller que eu consigo manipular os dados dentro dele
             controller: _pesoController,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
             decoration: InputDecoration(
               hintText: "Peso",
               border:
@@ -42,10 +93,19 @@ class _ImcState extends State<Imc> {
               icon: Icon(Icons.person),
             ),
           ),
+          Text(
+            '$_resultado',
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            '$_situacao',
+            style: TextStyle(fontSize: 30),
+          )
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.indigo,
+        onTap: _onItemTapped,
         items: [
           BottomNavigationBarItem(
               icon: Icon(Icons.clear, color: Colors.white),
