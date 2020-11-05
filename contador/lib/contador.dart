5fbc464785bc109581fbf1df6f4717feac661108
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-11-05 10:46:47 -0300

    Trabalhando com os estados da aplicação do flutter.

--- contador/lib/contador.dart
+++ contador/lib/contador.dart
@@ -7,19 +7,63 @@ class Contador extends StatefulWidget {
 }
 
 class _ContadorState extends State<Contador> {
+  var quantidade = 0;
+
+  incrementar() {
+    setState(() {
+      quantidade++;
+    });
+  }
+
+  decrementar() {
+    setState(() {
+      quantidade--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Contador"),
-        centerTitle: true,
-        backgroundColor: Colors.indigo,
-      ),
-      body: ListView(
-        children: <Widget>[
-
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text("Contador"),
+          centerTitle: true,
+          backgroundColor: Colors.indigo,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Card(
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(
+                      "Quantidade",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Text(
+                    "$quantidade",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                          onPressed: incrementar,
+                          child: Text(
+                            "Incrementar",
+                            style: TextStyle(fontSize: 20),
+                          )),
+                      FlatButton(
+                          onPressed: decrementar,
+                          child: Text(
+                            "Decrementar",
+                            style: TextStyle(fontSize: 20),
+                          ))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
